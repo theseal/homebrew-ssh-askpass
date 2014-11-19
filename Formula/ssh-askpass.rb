586388c43298c5129f27645cbae7deaab89dfe4d
Johan Carlquist <johan@rymdvarel.se>
2014-11-19 16:35:53 +0100

    New version - new default behavior.

--- Formula/ssh-askpass.rb
+++ Formula/ssh-askpass.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class SshAskpass < Formula
   homepage "https://github.com/theseal/ssh-askpass/"
-  url "https://github.com/theseal/ssh-askpass/archive/v1.0.0.tar.gz"
-  sha1 "92f5402e3b1ad3ceab3f07d18797bf3ab48fca80"
+  url "https://github.com/theseal/ssh-askpass/archive/v1.0.1.tar.gz"
+  sha1 "17b01f0eeba9257afc75a23654d28acf949cb3c8"
 
   def install
     bin.install "ssh-askpass"
