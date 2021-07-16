285d204078ca54f0f41795b0371c48b1c95c948b
Johan Wassberg <jocar@su.se>
2021-07-16 22:25:04 +0200

    New release from upstream

--- Formula/ssh-askpass.rb
+++ Formula/ssh-askpass.rb
@@ -1,7 +1,7 @@
 class SshAskpass < Formula
   desc "The ssh-askpass util for MacOS"
   homepage "https://github.com/theseal/ssh-askpass/"
-  url "https://github.com/theseal/ssh-askpass/archive/v1.2.2.tar.gz"
+  url "https://github.com/theseal/ssh-askpass/archive/v1.2.3.tar.gz"
   sha256 "a32e8bccbe44adddab3192cc4263d2c2d7f0fc2613ee80be4fcf46e233cea970"
 
   def install
