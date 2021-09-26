0c15d1098574dd55f0556628d0c535af19b49322
Johan Wassberg <johan@rymdvarel.se>
2021-09-26 13:20:48 +0200

    `brew services` requires a specific name in label
    Not sure if I makes this to complicated but I don't like having duplicate files
    in diffrent repos and don't want to take the Homebrew namespace to an external
    repo.
    
    Trying to fix https://github.com/theseal/ssh-askpass/issues/36 and it seems according to
    https://github.com/Homebrew/homebrew-services/issues/376
    that a service in brew needs to have the same name in label as the file name.

--- Formula/ssh-askpass.rb
+++ Formula/ssh-askpass.rb
@@ -4,6 +4,10 @@ class SshAskpass < Formula
   url "https://github.com/theseal/ssh-askpass/archive/v1.4.0.tar.gz"
   sha256 "9f13178d3856e7b280f8cc07bb7e755d8d1cd4ee4411fd48ffaa3235fcef9c32"
 
+  # The label in the plist must be #{plist_name} in order for `brew services` to work
+  # See https://github.com/Homebrew/homebrew-services/issues/376
+  patch :DATA
+
   def install
     bin.install "#{name}"
     prefix.install "#{name}.plist" => "#{plist_name}.plist"
@@ -19,3 +23,17 @@ class SshAskpass < Formula
     system "true"
   end
 end
+__END__
+diff --git a/ssh-askpass.plist b/ssh-askpass.plist
+index af9ab75..d74d0b8 100644
+--- a/ssh-askpass.plist
++++ b/ssh-askpass.plist
+@@ -3,7 +3,7 @@
+ <plist version="1.0">
+     <dict>
+         <key>Label</key>
+-        <string>com.github.theseal.ssh-askpass</string>
++        <string>homebrew.mxcl.ssh-askpass</string>
+         <key>LimitLoadToSessionType</key>
+         <string>Aqua</string>
+         <key>ProgramArguments</key>
