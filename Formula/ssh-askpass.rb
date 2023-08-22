d2e75c4717dc8d48f9a7208633cc2326a2fe34f8
Johan Wassberg <johan@rymdvarel.se>
2023-08-22 21:09:40 +0200

    `brew audit --strict --online`

--- Formula/ssh-askpass.rb
+++ Formula/ssh-askpass.rb
@@ -1,21 +1,23 @@
 class SshAskpass < Formula
-  desc "The ssh-askpass util for MacOS"
+  desc "Passphrase dialog for use with OpenSSH in macOS"
   homepage "https://github.com/theseal/ssh-askpass/"
   url "https://github.com/theseal/ssh-askpass/archive/v1.4.1.tar.gz"
   sha256 "a9a5439a422b544fb420fdbe77b3957a50137a509e31eb4fd00e57039749e009"
 
   def install
-    bin.install "#{name}"
+    bin.install name.to_s
     # The label in the plist must be #{plist_name} in order for `brew services` to work
     # See https://github.com/Homebrew/homebrew-services/issues/376
-    inreplace "ssh-askpass.plist", /<string>com.github.theseal.ssh-askpass<\/string>/, "<string>#{plist_name}</string>"
-    inreplace "ssh-askpass.plist", %r{/usr/local/bin}, "#{opt_prefix}/bin"
-    prefix.install "#{name}.plist" => "#{plist_name}.plist"
+    plist = "#{name}.plist"
+    inreplace plist, %r{<string>com.github.theseal.ssh-askpass</string>}, "<string>#{plist_name}</string>"
+    inreplace plist, %r{/usr/local/bin}, "#{opt_prefix}/bin"
+    prefix.install plist => "#{plist_name}.plist"
   end
 
-  def caveats; <<~EOF
-    NOTE: You will need to run the following to load the SSH_ASKPASS environment variable:
-      brew services start #{name}
+  def caveats
+    <<~EOF
+      NOTE: You will need to run the following to load the SSH_ASKPASS environment variable:
+        brew services start #{name}
     EOF
   end
 
