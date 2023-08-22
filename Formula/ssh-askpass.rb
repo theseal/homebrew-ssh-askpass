a7b07333a1a3c5f9df21c1b7da0cc907edd1dbf5
Johan Wassberg <johan@rymdvarel.se>
2023-08-22 20:34:03 +0200

    Bump version

--- Formula/ssh-askpass.rb
+++ Formula/ssh-askpass.rb
@@ -1,8 +1,8 @@
 class SshAskpass < Formula
   desc "The ssh-askpass util for MacOS"
   homepage "https://github.com/theseal/ssh-askpass/"
-  url "https://github.com/theseal/ssh-askpass/archive/v1.4.0.tar.gz"
-  sha256 "9f13178d3856e7b280f8cc07bb7e755d8d1cd4ee4411fd48ffaa3235fcef9c32"
+  url "https://github.com/theseal/ssh-askpass/archive/v1.4.1.tar.gz"
+  sha256 "a9a5439a422b544fb420fdbe77b3957a50137a509e31eb4fd00e57039749e009"
 
   def install
     bin.install "#{name}"
